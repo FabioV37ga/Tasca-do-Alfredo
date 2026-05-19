5aaa66a9491a9fd82fd30a0277f39216b0fe09d7
FabioV37ga <veigabfabio@hotmail.com>
2026-05-19 19:27:37 -0300

    Teste preload render.com

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,36 @@
 import { defineConfig } from 'vite';
-// import { resolve } from 'path';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: 'src',
+
   build: {
     outDir: '../dist',
     emptyOutDir: true,
+
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        sobre: resolve(__dirname, 'src/sobre.html'),
+        contato: resolve(__dirname, 'src/contato.html'),
+        cardapio: resolve(__dirname, 'src/cardapio.html'),
+        projeto: resolve(__dirname, 'src/projeto.html'),
+      },
+    },
   },
+
   server: {
     host: '0.0.0.0',
     port: 3210,
     open: true,
   },
+
   preview: {
     // Allow Render's host so `vite preview` accepts requests to that hostname, also, localhost for local testing
-    allowedHosts: ['tascadoalfredo.onrender.com', 'localhost', '0.0.0.0'],
+    allowedHosts: [
+      'tascadoalfredo.onrender.com',
+      'localhost',
+      '0.0.0.0',
+    ],
   },
 });
